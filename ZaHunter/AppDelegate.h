0069cb6a7559bd18865645c32b281afbbce123b4
GLB-MXM0004 <glb-mxm0004@MacBook-Pro-de-GLB-MXM0004.local>
2014-12-03 00:13:20 -0600

    Initial Commit

---
+++ ZaHunter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZaHunter
+//
+//  Created by GLB-MXM0004 on 03/12/14.
+//  Copyright (c) 2014 Hugo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZaHunter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZaHunter
+//
+//  Created by GLB-MXM0004 on 03/12/14.
+//  Copyright (c) 2014 Hugo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
